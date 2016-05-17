82c78db22e10df6783c775cfb5338f3733626bb8
hiroyuki shindo <withteeth666@gmail.com>
2016-05-17 14:28:31 +0900

    Third commit. fixes #1

--- src/java/HelloWorld.java
+++ src/java/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, GitHub!!!");
 		System.out.println("Second commit.");
+		System.out.println("issue close test.");
 	}
 }
